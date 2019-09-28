522ffe7043c20d057bcec67d4e5e099ab3c76334
souvikmaji <souvikmaji94@gmail.com>
2019-09-28 12:33:36 +0530

    csv file reading

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import csv
+from dateutil.parser import parse
+import argparse
+
+
+def get_file_name():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pathtocsv", help="path to the csv file containing stock information")
+    args = parser.parse_args()
+    return args.pathtocsv
+
+
 def main():
-    print("Hello World!")
+    filename = get_file_name()
+    print(filename)
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                print(f'Name: {row[0]}\t Date: {row[1]}\t Price: {row[2]}.')
+                line_count += 1
+        print(f'Processed {line_count} lines.')
 
 
 if __name__ == "__main__":
